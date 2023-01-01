d5b59dba7638b6d0ab3831209e59f61fa70f971a
Jishan2610 <75787027+Jishan2610@users.noreply.github.com>
2023-01-02 01:08:44 +0530

    Update Microsoft_Day1_1.java

--- Microsoft_Day1_1.java
+++ Microsoft_Day1_1.java
@@ -1,1 +1,37 @@
 
+import java.util.Stack;
+
+public class Microsoft_Day1{
+    public static void main(String[] args) {
+        //input-Output
+    }
+    class Solution {
+        public int evalRPN(String[] tokens) {
+            Stack<Integer> st=new Stack<>();
+            for(String s:tokens){
+                if(s.equals("+")){
+                    int res=st.pop()+st.pop();
+                    st.push(res);
+                }
+                else if(s.equals("-")){
+                    int res=-1*(st.pop()-st.pop());
+                    st.push(res);
+                }
+                else if(s.equals("/")){
+                    int a=st.pop();
+                    int b=st.pop();
+                    int res=b/a;
+                    st.push(res);
+                }
+                else if(s.equals("*")){
+                    int res=st.pop()*st.pop();
+                    st.push(res);
+                }
+                else{
+                    st.push(Integer.parseInt(s));
+                }
+            }
+            return st.pop();
+        }
+    }
+}
