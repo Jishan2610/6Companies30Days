382cfa5324e95dedfdbb254478796f95f2eab90d
Jishan2610 <75787027+Jishan2610@users.noreply.github.com>
2023-01-13 17:12:46 +0530

    Update Microsoft_Day5_1.java

---
+++ Microsoft_Day5_1.java
@@ -0,0 +1,10 @@
+public class Microsoft_Day5_1 {
+    public static void main(String[] args) {
+        
+    }
+    class Solution {
+        public double nthPersonGetsNthSeat(int n) {
+            return n == 1 ? 1.0 : 0.5;
+        }
+    }
+}
